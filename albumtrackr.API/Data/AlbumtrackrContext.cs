3fe58edd2170f6fcdb48d5c97713660f0e9226f2
ryandeering <ryandeering1@gmail.com>
2021-03-11 18:54:11 +0000

    API init

---
+++ albumtrackr.API/Data/AlbumtrackrContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace albumtrackr.API.Data
+{
+    public class AlbumtrackrContext : DbContext
+    {
+        public AlbumtrackrContext(DbContextOptions<AlbumtrackrContext> options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<StockListing> StockListing { get; set; } //change
+
+        //Add-Migration init
+        //Update-Database
+    }
+}
