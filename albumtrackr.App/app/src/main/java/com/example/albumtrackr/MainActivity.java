ee7531410130ea4920a7bdbbb98d0c82b47428da
JamesLynamX00073019 <X00073019@mytudublin.ie>
2021-03-14 15:50:40 +0000

    Created android volley app to call on an API, needs URI

--- albumtrackr.App/app/src/main/java/com/example/albumtrackr/MainActivity.java
+++ albumtrackr.App/app/src/main/java/com/example/albumtrackr/MainActivity.java
@@ -1,14 +1,113 @@
-package com.example.albumtrackr;
+// add Internet permission
 
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.albumtrackr;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.util.Log;
+import com.google.gson.*;
+import com.android.volley.*;
+import com.android.volley.toolbox.*;
+
+
+// in build.gradle for Module:app
+// implementation 'com.android.volley:volley:1.1.1'
+// implementation 'com.google.code.gson:gson:2.8.5'
+
 public class MainActivity extends AppCompatActivity {
+
+    // uri of RESTful service on Azure, note: https, cleartext support disabled by default
+    private String SERVICE_URI = "";          // or https
+    private String TAG = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // floating action button, call the service
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                callService(view);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // call RESTful service using volley and display results
+    public void callService(View v)
+    {
+        // get TextView for displaying result
+        final TextView outputTextView = (TextView) findViewById(R.id.outputTextView);
+
+        try
+        {
+            // make a string request (JSON request an alternative)
+            RequestQueue queue = Volley.newRequestQueue(this);
+            Log.d(TAG, "Making request");
+            try
+            {
+                StringRequest strObjRequest = new StringRequest(Request.Method.GET, SERVICE_URI,
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response)
+                            {
+                                // parse resulting string containing JSON to Greeting object
+                                Model model = new Gson().fromJson(response, Model.class);
+                                outputTextView.setText(model.toString());
+                                Log.d(TAG, "Displaying data" + model.toString());
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error)
+                            {
+                                outputTextView.setText(error.toString());
+                                Log.d(TAG, "Error" + error.toString());
+                            }
+                        });
+                queue.add(strObjRequest);           // can have multiple in a queue, and can cancel
+            }
+            catch (Exception e1)
+            {
+                Log.d(TAG, e1.toString());
+                outputTextView.setText(e1.toString());
+            }
+        }
+        catch (Exception e2)
+        {
+            Log.d(TAG, e2.toString());
+            outputTextView.setText(e2.toString());
+        }
     }
 }
---
+++ albumtrackr.App/app/src/main/java/com/example/albumtrackr/Model.java
@@ -0,0 +1,13 @@
+package com.example.albumtrackr;
+
+// MODEL CLASS - NAME WILL BE CHANGED
+public class Model
+{
+    private String message;
+    private String to;
+
+    public String toString()
+    {
+        return message + " " + to;
+    }
+}
