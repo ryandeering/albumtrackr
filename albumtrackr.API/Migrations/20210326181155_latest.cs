f717d697a68d419894d43a46fad1bf313f6e798f
ryandeering <ryandeering1@gmail.com>
2021-03-26 19:18:14 +0000

    Android: List displaying fully using Azure deployed API.

---
+++ albumtrackr.API/Migrations/20210326181155_latest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace albumtrackr.API.Migrations
+{
+    public partial class latest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
